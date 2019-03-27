6718352995a4440a052f5c2b61037f69022e8436
Adrian Rauchfleisch <adrian.rauchfleisch@gmail.com>
2019-03-28 03:27:44 +0800

    first...

---
+++ week_06/initLesson.R
@@ -0,0 +1,20 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+if(!require(stringr)){
+  install.packages("stringr")
+  library(stringr)
+}
+
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+load(file.path(.get_course_path(), "socne_week_06", "week_06", "tweets.rdata"))
+
+
